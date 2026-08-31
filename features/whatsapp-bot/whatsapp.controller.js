a427b4bc43d3f6e5def28b802c9e0c9fd282787e
Md Dilshan <imdilshan@proton.me>
2026-08-31 23:09:35 +0530

    Before migration

--- features/whatsapp-bot/whatsapp.controller.js
+++ features/whatsapp-bot/whatsapp.controller.js
@@ -1,9 +1,46 @@
+// const linkingController = require('./linking.controller');
+// const queryController = require('./query.controller');
+// const { sendWhatsAppMessage } = require('../../lib/twilio');
+
+// async function handleIncomingMessage(req, res) {
+//   const from = req.body.From; // e.g. 'whatsapp:+91XXXXXXXXXX'
+//   const body = req.body.Body || '';
+
+//   let reply;
+
+//   try {
+//     reply = await linkingController.handleLinkingFlow(from, body);
+
+//     if (reply === null) {
+//       reply = await queryController.handleQueryCommand(from, body);
+//     }
+//   } catch (err) {
+//     console.error('Error handling incoming WhatsApp message:', err);
+//     reply = `Something went wrong on our end. Please try again in a moment.`;
+//   }
+
+//   try {
+//     await sendWhatsAppMessage(from, reply);
+//   } catch (err) {
+//     console.error('Error sending WhatsApp reply:', err);
+//   }
+
+//   res.status(200).send('<Response></Response>');
+// }
+
+// module.exports = { handleIncomingMessage };
+
 const linkingController = require('./linking.controller');
 const queryController = require('./query.controller');
-const { sendWhatsAppMessage } = require('../../lib/twilio');
+
+function escapeXml(str) {
+  return str.replace(/[<>&'"]/g, (c) => ({
+    '<': '&lt;', '>': '&gt;', '&': '&amp;', "'": '&apos;', '"': '&quot;',
+  }[c]));
+}
 
 async function handleIncomingMessage(req, res) {
-  const from = req.body.From; // e.g. 'whatsapp:+91XXXXXXXXXX'
+  const from = req.body.From;
   const body = req.body.Body || '';
 
   let reply;
@@ -19,13 +56,8 @@ async function handleIncomingMessage(req, res) {
     reply = `Something went wrong on our end. Please try again in a moment.`;
   }
 
-  try {
-    await sendWhatsAppMessage(from, reply);
-  } catch (err) {
-    console.error('Error sending WhatsApp reply:', err);
-  }
-
-  res.status(200).send('<Response></Response>');
+  res.set('Content-Type', 'text/xml');
+  res.status(200).send(`<Response><Message>${escapeXml(reply)}</Message></Response>`);
 }
 
 module.exports = { handleIncomingMessage };
